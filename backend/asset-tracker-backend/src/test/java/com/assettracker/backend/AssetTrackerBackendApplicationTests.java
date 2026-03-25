48d210c0c8140f916fa6766dddf04f5677e23a5a
DaeSoeps <thdeotjq12@naver.com>
2026-03-25 19:24:54 +0900

    Initial backend spring boot project

---
+++ backend/asset-tracker-backend/src/test/java/com/assettracker/backend/AssetTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assettracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
